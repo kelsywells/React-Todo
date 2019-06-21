8713ae9993167268767ae1bbd4a5939b6039d53a
Kelsy <kelsywells@gmail.com>
2019-06-20 18:36:31 -0700

    input working, submit errors have gone away, text not showing up yet

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 //TODO INPUT FIELD, 'ADD' BUTTON, 'TASK COMPLETED' BUTTON
 
@@ -10,15 +9,33 @@ class TodoForm extends React.Component {
         }
     }
 
+    addTask = (newItem) => {
+        const newTask = {
+          task: '',
+          id: Date.now(),
+          completed: false
+        }
+    
+        this.setState(
+             {currentToDos: [...this.state.currentToDos, newTask]}
+        )
+      }
+
     changeHandler = (event) => {
         this.setState(
             {task: event.target.value}
         )
     }
 
+    submitHandler = (event) => {
+        event.preventDefault()
+        this.props.addNewTask(this.state.task);
+        console.log('event:', this.submitHandler)
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit = {this.submitHandler}>
                 <input value={this.state.task} placeholder='New Task' type='text' onChange={this.changeHandler} />
                 <button>Add Task</button>
             </form>
@@ -29,27 +46,3 @@ class TodoForm extends React.Component {
 
 
 export default TodoForm;
-=======
-// - `<TodoForm>` will hold your input field and your `Add Todo` and `Clear Completed` buttons.
-//   - Your input field should take in user input, and allow a user to press `Enter` or click on the `Submit Button` to add a todo to your list.
-//   - Once a todo is submitted, the Todo List should re-render and show the added todo.
-
-import React from 'react';
-import TodoList from './TodoList'
-
-function TodoForm(props) {
-    return (
-    <form /*onSubmit={props.handle}*/>
-        <input type="text" name="todo" placeholder="...todo" />
-        <button>Add Todo</button>
-        <button>Clear Completed</button>
-    </form>
-    );
-}
-
-// function handleChangeFunction(event) {
-//     this.setState({task: event.target.value});
-// }
-//     <input onChange={this.handleChangeFunction} />
-export default TodoForm; 
->>>>>>> 55852ce674aa3857a0b74344735e79720aeb997c
